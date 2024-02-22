ecf6dc929680c4345532162ceba2dc4cb4ffc292
Coramoli <147661467+Coramoli@users.noreply.github.com>
2024-02-22 13:22:34 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//1º ejercicio
+console.log("Hola mundo")
+
+//2º ejercicio
+const nombre = "maria";
+const edad =40;
+const mayorEdad= true;
+const direccion= {calle: "Doctor Barraquer", numero:"19", codigoPortal:"28200"}
+const coloresFavoritos=["morado", "rojo", "verde", "azul"]
+const lenguajesFavoritos=["html", "css", "javaScrip"]
+const mejorLenguaje= lenguajesFavoritos[0]
+const peorLenguaje= lenguajesFavoritos[2]
+
+//3º ejercicio
+let numero1= 7;
+let numero2= 3;
+let resultado= numero1 + numero2;
+console.log(resultado);
+
+let minuendo= 15;
+let sustraendo= 5;
+let resultado2= minuendo - sustraendo;
+console.log(resultado2);
+
+let factor1= 7;
+let factor2= 9;
+let resultado3= factor1*factor2;
+console.log(resultado3);
+
+let dividendo= 1000;
+let divisor= 10;
+let resultado4= dividendo / divisor;
+console.log(resultado4);
+
+//4º ejercicio
+const saludo = "Hola"
+if (saludo === "Hola"){
+    console.log("Ha saludado")
+}
+
+//5º elercicio
+const pokemon = ["Charizard", "Dragonite", "Pikachu"];
+console.log ([0],[1],[2]);
+
+//mostrar cada pokemon en una linea
+console.log([0])
+console.log ([1])
+console.log([2])
+
+//6º ejercicio
+//const pokemon = ["pokemon1", "pokemon2", "pokemon3"];
+//const pokemon1 = {nombre: "Charizard", tipoDePokemon: "fuego"};
+//const pokemon2 = {nombre:"Dragonite", tipoDePokemon:"agua"};
+//const pokemon3 = {nombre:"Pikachu", tipoDePokemon:"aire"};
+//if (pokemon==fuergo){
+//    console.log("Es un pokemon de fuego")}
+
+//Boolean
+for
